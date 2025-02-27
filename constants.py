814c410e74b21ff5da3578e6256866651da68f32
MRX-lab09 <sonyagym@yandex.ru>
2025-02-28 00:15:50 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,15 +5,21 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 FPS = 60
-COLOR = (0, 255, 0)
+
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
-BALL_SIZE = 20
-L_PADDLE_SPEED = 15
-R_PADDLE_SPEED = 4
-BALL_SPEED_X, BALL_SPEED_Y = 6, 6
+BUTTON_WIDTH, BUTTON_HEIGHT = 200, 50
+BUTTON_X = WIDTH // 2 - BUTTON_WIDTH // 2
+
+BALL_SPEED_X = 6
+BALL_SPEED_Y = 6
+PADDLE_LEFT_SPEED = 15
+PADDLE_RIGHT_SPEED = 4
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Пинг-Понг")
+clock = pygame.time.Clock()
